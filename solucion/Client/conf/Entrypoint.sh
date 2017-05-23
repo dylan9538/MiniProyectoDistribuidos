670eac04f589a52aec5dc28e1ab2335101f60661
AndresPineros <andres.pineros@hotmail.com>
2017-05-23 18:23:47 -0500

    avances

--- solucion/Client/conf/Entrypoint.sh
+++ solucion/Client/conf/Entrypoint.sh
@@ -12,6 +12,6 @@ echo "deb http://mirror_c:8080/ xenial main" > /etc/apt/sources.list
 chmod 777 /tmp
 apt-get clean
 apt-get update -y
-apt-get install postgresql -y
+apt-get install python3 -y
 
-/etc/init.d/postgresql start
+postgres
