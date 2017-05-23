ffdea9d3df43b925e68a3f587bf2acc983915734
AndresPineros <andres.pineros@hotmail.com>
2017-05-22 22:30:59 -0500

    avances

---
+++ solucion/Mirror/conf/Entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IFS=', ' read -r -a array <<< "${1}"
+
+echo "${1}"
+echo "${array[@]}"
+
+symbol=" | "
+mirrorFilter="Priority (required) | Priority (important) | Priority (standard) "
+
+for package in "${array[@]}"
+do
+ mirrorFilter=$mirrorFilter$symbol$package
+done
+
+echo "$mirrorFilter"
+
+aptly mirror edit -filter="$mirrorFilter" mirror-xenial
+aptly mirror update mirror-xenial
+aptly snapshot create mirror-snap-xenial from mirror mirror-xenial
+./scripts/publish_snapshot.sh
+
+aptly serve
+
